77348ca753f53bdf1b1c9e6e1afefeb82e7c44dd
nickeisenberg <nickeisenberg@gmail.com>
2025-02-21 13:04:32 -0800

    tools to use local repo

--- src/variables.sh
+++ src/variables.sh
@@ -1,14 +1,27 @@
 #--------------------------------------------------
 # venvman
 #--------------------------------------------------
-export VENVMAN_ROOT_DIR=$HOME/.venvman
+set_if_not_exists() {
+    if [ -z $(eval "echo \$$1") ]; then
+        eval "${1}=${2}"
+    fi
+}
 
-export VENVMAN_ENVS_DIR=${VENVMAN_ROOT_DIR}/envs
+set_if_not_exists "VENVMAN_ROOT_DIR" "${HOME}/.venvman"
+export VENVMAN_ROOT_DIR
 
-export VENVMAN_SRC_DIR=${VENVMAN_ROOT_DIR}/venvman/src
+set_if_not_exists "VENVMAN_ENVS_DIR" "${HOME}/.venvman/envs"
+export VENVMAN_ENVS_DIR
+
+set_if_not_exists "VENVMAN_SRC_DIR" "${HOME}/.venvman/venvman/src"
+export VENVMAN_SRC_DIR
 export VENVMAN_UTILS_DIR=${VENVMAN_SRC_DIR}/venvman/utils
 export VENVMAN_COMMANDS_DIR=${VENVMAN_SRC_DIR}/venvman/commands
 
 export VENVMAN_CPYTHON_REMOTE_URL="https://github.com/python/cpython.git"
-export VENVMAN_CPYTHON_REPO_DIR=${VENVMAN_ROOT_DIR}/cpython
-export VENVMAN_PYTHON_BUILDS_DIR=${VENVMAN_ROOT_DIR}/builds
+
+set_if_not_exists "VENVMAN_CPYTHON_REPO_DIR" "${HOME}/.venvman/cpython"
+export VENVMAN_CPYTHON_REPO_DIR
+
+set_if_not_exists "VENVMAN_PYTHON_BUILDS_DIR" "${HOME}/.venvman/builds"
+export VENVMAN_PYTHON_BUILDS_DIR
